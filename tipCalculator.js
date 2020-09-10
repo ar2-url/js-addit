3dcb06df7a1e0fa019ea15362916f0bb93748df3
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-09-10 21:41:54 +0100

    Add files via upload

---
+++ tipCalculator.js
@@ -0,0 +1,39 @@
+const tipCalculator = (quality, total) => {
+    switch (quality) {
+        case 'bad':
+            return total * .05;
+        case 'ok':
+            return total * .15;
+        case 'good':
+            return total * .20;
+        case 'excellent':
+            return total * .30;
+        default:
+            return total * .18;
+
+    }
+}
+/*
+// As a function declaration:
+function tipCalculator(quality, total) {
+    switch (quality) {
+        case 'bad':
+            return total * .05;
+        case 'ok':
+            return total * .15;
+        case 'good':
+            return total * .20;
+        case 'excellent':
+            return total * .30;
+        default:
+            return total * .18;
+
+    }
+}
+*/
+console.log(tipCalculator('good', 100))
+
+
+//but even bad tip from 400 looks nice :)
+//console.log(tipCalculator('bad', 400))
+//should return £20
